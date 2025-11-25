d95d6dc31e4530abcf63fa7d7612a010a54bab9e
youness <youness.ouahmane4@gmail.com>
2025-11-25 10:47:19 +0100

    base project structure

---
+++ src/main/java/org/smartshop/smartshop/SmartshopApplication.java
@@ -0,0 +1,15 @@
+package org.smartshop.smartshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SmartshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartshopApplication.class, args);
+    }
+
+}
