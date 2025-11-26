9ce6eb473434dd98badab0072947e3ae9ae93d0c
youness <youness.ouahmane4@gmail.com>
2025-11-26 11:00:37 +0100

    add client request, resposne and update dtos

--- src/main/java/org/smartshop/smartshop/dto/clientdto/ClientResponseDTO.java
+++ src/main/java/org/smartshop/smartshop/dto/clientdto/ClientResponseDTO.java
@@ -1,4 +1,26 @@
 package org.smartshop.smartshop.dto.clientdto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.smartshop.smartshop.enums.CustomerTier;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ClientResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private CustomerTier tier;
+    private Integer totalOrders;
+    private BigDecimal totalSpent;
+    private LocalDateTime firstOrderDate;
+    private LocalDateTime lastOrderDate;
+    private String username;
 }
