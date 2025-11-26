9bb59acef6cac0f6dc9e796044962df502975b93
youness <youness.ouahmane4@gmail.com>
2025-11-26 11:37:35 +0100

    add payment dtos

---
+++ src/main/java/org/smartshop/smartshop/dto/payment/PaymentResponseDTO.java
@@ -0,0 +1,29 @@
+package com.smartshop.dto.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.smartshop.smartshop.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentResponseDTO {
+
+    private Long id;
+    private Integer paymentNumber;
+    private BigDecimal amount;
+    private String paymentMethod;
+    private LocalDate paymentDate;
+    private LocalDate cashDate;
+    private PaymentStatus status;
+    private String reference;
+    private String bankName;
+    private LocalDate dueDate;
+
+}
