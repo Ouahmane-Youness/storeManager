a3a33855836739b4ce6ef1d4c73396016666c93f
youness <youness.ouahmane4@gmail.com>
2025-11-27 09:46:34 +0100

    add product, promocode and payment mapper

--- src/main/java/org/smartshop/smartshop/mapper/PaymentMapper.java
+++ src/main/java/org/smartshop/smartshop/mapper/PaymentMapper.java
@@ -1,4 +1,24 @@
 package org.smartshop.smartshop.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.smartshop.smartshop.dto.payment.PaymentResponseDTO;
+import org.smartshop.smartshop.entity.Payment;
+import org.smartshop.smartshop.dto.payment.CreatePaymentRequestDTO;
+import java.util.List;
+
+
+@Mapper(componentModel = "spring")
 public interface PaymentMapper {
+
+    PaymentResponseDTO toResponseDTO(Payment payment);
+
+    List<PaymentResponseDTO> toResponseDTOList(List<Payment> payments);
+
+    @Mapping(target = "paymentNumber", ignore = true)
+    @Mapping(target = "cashDate", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "order", ignore = true)
+    Payment toEntity(CreatePaymentRequestDTO dto);
+
 }
--- src/main/java/org/smartshop/smartshop/mapper/ProductMapper.java
+++ src/main/java/org/smartshop/smartshop/mapper/ProductMapper.java
@@ -1,4 +1,30 @@
 package org.smartshop.smartshop.mapper;
 
-public class ProductMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.smartshop.smartshop.dto.productdto.CreateProductRequestDTO;
+import org.smartshop.smartshop.dto.productdto.ProductResponseDTO;
+import org.smartshop.smartshop.dto.productdto.UpdateProductRequestDTO;
+import org.smartshop.smartshop.entity.Product;
+
+import java.util.List;
+
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    ProductResponseDTO toResponseDTO(Product product);
+
+    List<ProductResponseDTO> toResponseDTOList(List<Product> products);
+
+    Product toEntity(CreateProductRequestDTO dto);
+
+    @Mapping(target = "name", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "price", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "stock", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "active", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDTO(UpdateProductRequestDTO dto, @MappingTarget Product product);
+
 }
